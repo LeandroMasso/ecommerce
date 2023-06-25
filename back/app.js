c128e0fd1cac4361970e30e7f878169a47b788a9
LeandroMasso <leandromasso88@gmail.com>
2023-06-25 14:05:33 -0300

    Entrega TP1 modulos 2 y 3

--- back/app.js
+++ back/app.js
@@ -5,9 +5,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const usersRouter = require('./routes/users');
-const usersRouter = require('./routes/users');
+const detailProduct = require('./routes/detailProduct');
+const login = require('./routes/login');
+const register = require('./routes/register');
+const cart = require('./routes/cart');
 
 const app = express();
 
@@ -22,7 +23,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/detailProduct', detailProduct);
+app.use('/login', login);
+app.use('/register', register);
+app.use('/cart', cart);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
